10abf77819316ef1ef994fe92e682aabba5e6b1c
Coboy16 <smautrap.16@gmail.com>
2026-02-03 22:01:20 -0500

    feat(features): export all features

---
+++ lib/features/features.dart
@@ -0,0 +1,7 @@
+// Barrel file for all features
+export 'auth/auth.dart';
+export 'accounts/accounts.dart';
+export 'transactions/transactions.dart';
+export 'transfers/transfers.dart';
+export 'cards/cards.dart';
+export 'shared/shared.dart';
